d11b80b12683ef4ab6414ac7e41170466373c3d6
3xp1o1t <jes.monti.503@gmail.com>
2023-07-19 13:16:37 -0600

    Added rapidapi cars consumation
    Added carcard to show car basic details
    Added a button to show more details of the car
    Added util folder for help functions
    That secret key on index.ts at utils it's ok for public.

---
+++ utils/index.ts
@@ -0,0 +1,31 @@
+export async function fetchCars() {
+  const headers = {
+    "X-RapidAPI-Key": "ec516461b2msh1f047b68a25bc66p11a99bjsnd89804051cf6",
+    "X-RapidAPI-Host": "cars-by-api-ninjas.p.rapidapi.com",
+  };
+
+  const response = await fetch(
+    "https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla",
+    {
+      headers: headers,
+    }
+  );
+
+  const result = await response.json();
+  return result;
+}
+
+export const calculateCarRent = (city_mpg: number, year: number) => {
+  const basePricePerDay = 50; // price in dollars
+  const mileageFactor = 0.1; // additional rate per mile driven
+  const ageFactor = 0.05; // additional rate per year of vehicle age
+
+  //calculate rate based on mileage and age.
+  const mileageRate = city_mpg * mileageFactor;
+  const ageRate = (new Date().getFullYear() - year) * ageFactor;
+
+  // Calculate total rental x day
+  const rentalRatePerDay = basePricePerDay + mileageFactor + ageFactor;
+
+  return rentalRatePerDay.toFixed(0);
+};
